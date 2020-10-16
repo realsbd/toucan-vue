da318df5d4da25411bdeeb8ab6dd216309895d27
aspiredev <abimbola.salawudeen@gmail.com>
2020-10-16 05:04:30 -0400

    route fixed

---
+++ src/router/starter.js
@@ -0,0 +1,68 @@
+/*
+ * Starter Router
+ */
+
+// Vue and Vue Router
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// Main layouts
+import LayoutBackend from '@/layouts/variations/BackendStarter.vue'
+import LayoutSimple from '@/layouts/variations/Simple.vue'
+
+// Register Vue Router
+Vue.use(Router)
+
+// Frontend Page Example
+const Landing = () => import("@/views/starter/Landing.vue")
+
+// Backend Page Example
+const Dashboard = () => import("@/views/starter/Dashboard.vue")
+const Impact = () => import("@/views/starter/Simple1.vue")
+
+// Router Configuration
+export default new Router({
+  linkActiveClass: 'active',
+  linkExactActiveClass: '',
+  scrollBehavior () {
+    return { x: 0, y: 0 }
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/landing',
+      component: LayoutSimple,
+      children: [
+        {
+          path: '/landing',
+          name: 'Landing',
+          component: Landing
+        }
+      ]
+    },
+    {
+      path: '/backend',
+      redirect: '/backend/dashboard',
+      component: LayoutBackend,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: Dashboard
+        }
+      ]
+    },
+    {
+      path: '/backend',
+      redirect: '/backend/impact',
+      component: LayoutBackend,
+      children: [
+        {
+          path: 'impact',
+          name: 'Impact',
+          component: Impact
+        }
+      ]
+    }
+  ]
+})
