e286c2db2d2fb7d7094e78d4aad73fb3403981cc
aspiredev <abimbola.salawudeen@gmail.com>
2020-10-16 08:06:12 -0400

    Global goals done

--- src/views/starter/GlobalGoals.vue
+++ src/views/starter/GlobalGoals.vue
@@ -1,56 +1,66 @@
 <template>
   <div>
-    <!-- Hero -->
-    <base-page-heading title="Responsive Tables" subtitle="Mobile friendly tables that work across all screen sizes.">
-      <template #extra>
-        <b-breadcrumb class="breadcrumb-alt">
-          <b-breadcrumb-item href="javascript:void(0)">Tables</b-breadcrumb-item>
-          <b-breadcrumb-item active>Global Goals</b-breadcrumb-item>
-        </b-breadcrumb>
-      </template>
-    </base-page-heading>
-    <!-- END Hero -->
-
     <!-- Page Content -->
     <div class="content">
       <!-- Full Table -->
       <base-block rounded title="Full Table">
         <template #options>
-          <button type="button" class="btn-block-option">
-            <i class="si si-settings"></i>
-          </button>
+          <!-- Default Variation -->
+          <base-block rounded>
+            <b-button variant="alt-primary" class="push" v-b-modal.modal-block-normal>
+              Add New Global Goal
+            </b-button>
+          </base-block>
+          <!-- END Default Variation -->
+
+          <!-- Normal Block Modal -->
+          <b-modal id="modal-block-normal" body-class="p-0" hide-footer hide-header>
+            <div class="block block-rounded block-themed block-transparent mb-0">
+              <div class="block-header bg-primary-dark">
+                <h3 class="block-title">Add New Global Goal</h3>
+                <div class="block-options">
+                  <button type="button" class="btn-block-option" @click="$bvModal.hide('modal-block-normal')">
+                    <i class="fa fa-fw fa-times"></i>
+                  </button>
+                </div>
+              </div>
+                <b-form class="block-content font-size-sm" @submit="onSubmit">
+                  <b-form-group label="Global Goal" label-for="Add new Global Goal">
+                    <b-form-input id="globalGoal" name="globalGoal" placeholder="Add new Global Goal"></b-form-input>
+                  </b-form-group>
+                  <b-form-group>
+                    <b-button type="submit" size="sm" variant="primary">
+                      Submit
+                    </b-button>
+                  </b-form-group>
+                </b-form>
+            </div>
+          </b-modal>
+          <!-- END Normal Block Modal -->
         </template>
-        <p class="font-size-sm text-muted">
-          The first way to make a table responsive is to add the <code>responsive</code> property. This way, the table will be horizontally scrollable and all data will be accessible on smaller screens.
-          You could also specify a value to apply the horizontal scrolling on different screen widths: <code>sm</code>, <code>md</code>, <code>lg</code>, <code>xl</code>.
-        </p>
         <b-table-simple responsive bordered striped table-class="table-vcenter">
           <b-thead>
             <b-tr>
               <b-th class="text-center" style="width: 100px;">
                 <i class="far fa-user"></i>
               </b-th>
-              <b-th>Name</b-th>
-              <b-th style="width: 30%;">Email</b-th>
-              <b-th style="width: 15%;">Access</b-th>
+              <b-th>Label</b-th>
+              <b-th style="width: 30%;">Link</b-th>
               <b-th class="text-center" style="min-width: 110px; width: 110px;">Actions</b-th>
             </b-tr>
           </b-thead>
           <b-tbody>
-            <b-tr v-for="user in users" :key="user.id">
+            <b-tr v-for="globalGoal in globalGoals" :key="globalGoal.id">
               <b-td class="text-center">
-                <img class="img-avatar img-avatar48" :src="`img/avatars/${user.avatar}.jpg`" alt="Avatar">
+                <img class="img-avatar img-avatar48" :src="`img/avatars/${globalGoal.avatar}.jpg`" alt="Avatar">
               </b-td>
               <b-td class="font-w600 font-size-sm">
-                <a :href="`${user.href}`">
-                  {{ user.name }}
+                <a :href="`${globalGoal.href}`">
+                  {{ globalGoal.label }}
                 </a>
               </b-td>
               <b-td class="font-size-sm">
-                client{{ user.id }}<em class="text-muted">@example.com</em>
-              </b-td>
-              <b-td>
-                <b-badge :variant="user.labelVariant">{{ user.labelText }}</b-badge>
+                client{{ globalGoal.id }}<em class="text-muted">@example.com</em>
               </b-td>
               <b-td class="text-center">
                 <b-btn-group>
@@ -67,61 +77,6 @@
         </b-table-simple>
       </base-block>
       <!-- END Full Table -->
-
-      <!-- Partial Table -->
-      <base-block rounded title="Partial Table">
-        <template #options>
-          <button type="button" class="btn-block-option">
-            <i class="si si-settings"></i>
-          </button>
-        </template>
-        <p class="font-size-sm text-muted">
-          The second way is to use <router-link to="/backend/elements/grid">responsive utility CSS classes</router-link> for hiding columns in various screen resolutions. This way you can hide less important columns and keep the most valuable on smaller screens.
-          At the following example the <strong>Access</strong> column isn't visible on small and extra small screens and <strong>Email</strong> column isn't visible on extra small screens.
-        </p>
-        <b-table-simple bordered striped table-class="table-vcenter">
-          <b-thead>
-            <b-tr>
-              <b-th class="text-center" style="width: 100px;">
-                <i class="far fa-user"></i>
-              </b-th>
-              <b-th>Name</b-th>
-              <b-th class="d-none d-sm-table-cell" style="width: 30%;">Email</b-th>
-              <b-th class="d-none d-md-table-cell" style="width: 15%;">Access</b-th>
-              <b-th class="text-center" style="min-width: 110px; width: 110px;">Actions</b-th>
-            </b-tr>
-          </b-thead>
-          <b-tbody>
-            <b-tr v-for="user in users" :key="user.id">
-              <b-td class="text-center">
-                <img class="img-avatar img-avatar48" :src="`img/avatars/${user.avatar}.jpg`" alt="Avatar">
-              </b-td>
-              <b-td class="font-w600 font-size-sm">
-                <a :href="`${user.href}`">
-                  {{ user.name }}
-                </a>
-              </b-td>
-              <b-td class="d-none d-sm-table-cell font-size-sm">
-                client{{ user.id }}<em class="text-muted">@example.com</em>
-              </b-td>
-              <b-td class="d-none d-md-table-cell">
-                <b-badge :variant="user.labelVariant">{{ user.labelText }}</b-badge>
-              </b-td>
-              <b-td class="text-center">
-                <b-btn-group>
-                  <b-button size="sm" variant="alt-primary">
-                    <i class="fa fa-fw fa-pencil-alt"></i>
-                  </b-button>
-                  <b-button size="sm" variant="alt-primary">
-                    <i class="fa fa-fw fa-times"></i>
-                  </b-button>
-                </b-btn-group>
-              </b-td>
-            </b-tr>
-          </b-tbody>
-        </b-table-simple>
-      </base-block>
-      <!-- END Partial Table -->
     </div>
     <!-- END Page Content -->
   </div>
@@ -131,10 +86,10 @@
 export default {
   data () {
     return {
-      users: [
+      globalGoals: [
         {
           id: 1,
-          name: 'Adam McCoy',
+          label: 'Adam McCoy',
           avatar: 'avatar10',
           href: 'javascript:void(0)',
           labelVariant: 'success',
@@ -142,7 +97,7 @@ export default {
         },
         {
           id: 2,
-          name: 'Betty Kelley',
+          label: 'Betty Kelley',
           avatar: 'avatar2',
           href: 'javascript:void(0)',
           labelVariant: 'info',
@@ -150,7 +105,7 @@ export default {
         },
         {
           id: 3,
-          name: 'Jesse Fisher',
+          label: 'Jesse Fisher',
           avatar: 'avatar9',
           href: 'javascript:void(0)',
           labelVariant: 'primary',
@@ -158,7 +113,7 @@ export default {
         },
         {
           id: 4,
-          name: 'Ryan Flores',
+          label: 'Ryan Flores',
           avatar: 'avatar12',
           href: 'javascript:void(0)',
           labelVariant: 'warning',
@@ -166,7 +121,7 @@ export default {
         },
         {
           id: 5,
-          name: 'Alice Moore',
+          label: 'Alice Moore',
           avatar: 'avatar4',
           href: 'javascript:void(0)',
           labelVariant: 'danger',
